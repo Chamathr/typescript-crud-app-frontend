8e85870992442a25c7f1b1523b3e30f648860dfd
chamathr <chamatht20@gmailcom>
2023-04-13 21:56:05 +0530

    changess

--- src/components/data/View.tsx
+++ src/components/data/View.tsx
@@ -1,94 +1,62 @@
-import * as React from 'react';
-import {
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-    Paper,
-    TablePagination,
-    colors
-} from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchData } from '../../redux/actions/dataAction';
+import { Table, Button } from 'antd';
+
 
 const View = (props: any) => {
 
     const { dataList } = props
     const dataPerPage = 2
-    
+
     const dispatch = useDispatch();
 
-    const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(dataPerPage);
+    const [page, setPage] = useState(1);
+
+    const handlePageChange = (pageNumber: number) => {
+        setPage(pageNumber)
+    };
 
     useEffect(() => {
-        dispatch(fetchData(page + 1))
-    },[page])
+        dispatch(fetchData(page))
+    }, [page]);
 
     const columns = [
-        { id: 'name', label: 'Name' },
-        { id: 'email', label: 'Email' },
-        { id: 'age', label: 'Age' }
+        { title: 'Name', dataIndex: 'name', key: 'name' },
+        { title: 'Age', dataIndex: 'age', key: 'age' },
+        { title: 'Email', dataIndex: 'email', key: 'email' },
+        {
+            title: 'Actions',
+            key: 'actions',
+            render: (text: any, record: any) => (
+                <>
+                    <Button type="primary" onClick={() => console.log(record)}>
+                        View Details
+                    </Button>
+                    <Button type="primary" onClick={() => console.log(record)} style={{marginLeft: '10px'}}>
+                        Edit
+                    </Button>
+                    <Button type="primary" onClick={() => console.log(record)} style={{marginLeft: '10px'}}>
+                        Delete
+                    </Button>
+                </>
+            ),
+        }
     ];
 
-    const handleChangePage = (event: any, newPage: any) => {
-        setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (event: any) => {
-        setRowsPerPage(event.target.value);
-        setPage(0);
-    };
-
     return (
-        <Paper sx={{ width: '100%' }}>
-            <TableContainer sx={{ maxHeight: 440 }}>
-                <Table stickyHeader aria-label="sticky table" sx={{ backgroundColor: '#F0FFFF' }}>
-                    <TableHead>
-                        <TableRow>
-                            {columns.map((column) => (
-                                <TableCell
-                                    key={column.id}
-                                    style={{ top: 10 }}
-                                >
-                                    {column.label}
-                                </TableCell>
-                            ))}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {dataList?.data
-                            ?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                            .map((row: any) => {
-                                return (
-                                    <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                                        {columns.map((column) => {
-                                            const value = row[column.id];
-                                            return (
-                                                <TableCell>
-                                                    {value}
-                                                </TableCell>
-                                            );
-                                        })}
-                                    </TableRow>
-                                );
-                            })}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <TablePagination
-                rowsPerPageOptions={[dataPerPage]}
-                component="div"
-                count={dataList?.totalItems}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
+        <>
+            <Table
+                columns={columns}
+                dataSource={dataList?.data}
+                pagination={{
+                    total: dataList?.totalItems,
+                    pageSize: dataPerPage,
+                    pageSizeOptions: [5, 10, 20],
+                    onChange: handlePageChange,
+                }}
             />
-        </Paper>
+        </>
     )
 }
 
