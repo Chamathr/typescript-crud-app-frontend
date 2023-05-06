a89023232b6b9a57deab78d417b31cd01423c44a
chamathr <chamatht20@gmailcom>
2023-05-06 18:03:27 +0530

    changes

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import authentication from "./auth/b2cAuth";
+
+authentication.initialize();
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+authentication.run(() => {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+})
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
